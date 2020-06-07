a00c59f9b8eaf2853fcc397fef31982dd862d314
ad123412 <ad123412@gmail.com>
2020-06-07 11:21:48 +0530

    multiple client credentials added

--- src/main/java/com/ada/oauth2/config/OAuth2AuthorizationServer.java
+++ src/main/java/com/ada/oauth2/config/OAuth2AuthorizationServer.java
@@ -3,17 +3,24 @@ package com.ada.oauth2.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.builders.ClientDetailsServiceBuilder;
+import org.springframework.security.oauth2.config.annotation.builders.InMemoryClientDetailsServiceBuilder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 @Configuration
 public class OAuth2AuthorizationServer extends AuthorizationServerConfigurerAdapter {
+
+    private static final Map<String, String> CLIENTS = new HashMap<String, String>() {{
+        put("ada-client", "ada-secret");
+        put("another-client", "another-secret");
+    }};
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -25,12 +32,36 @@ public class OAuth2AuthorizationServer extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory()
-                .withClient("ada-client")
-                .secret(passwordEncoder().encode("ada-secret"))
-                .authorizedGrantTypes("client_credentials")
-                .accessTokenValiditySeconds(60)
-                .scopes("resource-server-read", "resource-server-write");
+        ClientDetailsServiceBuilder<InMemoryClientDetailsServiceBuilder>.ClientBuilder builder =
+                clients.inMemory()
+                        .withClient("mobile-client")
+                        .secret(passwordEncoder().encode("mobile-secret"))
+                        .authorizedGrantTypes("client_credentials")
+                        .accessTokenValiditySeconds(60)
+                        .scopes("resource-server-read", "resource-server-write");
+
+        Iterator<Map.Entry<String, String>> clientEntryIterator = CLIENTS.entrySet().iterator();
+        composeBuilder(builder, clientEntryIterator);
+    }
+
+    private ClientDetailsServiceBuilder<InMemoryClientDetailsServiceBuilder>.ClientBuilder
+    composeBuilder(ClientDetailsServiceBuilder<InMemoryClientDetailsServiceBuilder>.ClientBuilder builder,
+                   Iterator<Map.Entry<String, String>> iterator) {
+        if (iterator.hasNext()) {
+            Map.Entry<String, String> entry = iterator.next();
+            builder
+                    .and()
+                    .withClient(entry.getKey())
+                    .secret(passwordEncoder().encode(entry.getValue()))
+                    .authorizedGrantTypes("client_credentials")
+                    .accessTokenValiditySeconds(60)
+                    .scopes("resource-server-read", "resource-server-write");
+        }
+        if (iterator.hasNext()) {
+            return composeBuilder(builder, iterator);
+        } else {
+            return builder;
+        }
     }
 
 //    @Override
